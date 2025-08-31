dabb9f3e1d0374c9013b776cc5393d13aada89bf
saicharanb2h <saicharanb3h@gmail.com>
2025-08-31 23:55:54 +0530

    Basics of git

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void fullName(String firstname, String LastName) =>
+    print("$firstname $LastName");
+
+void main() {
+  runApp(MyApp());
+}
+
+// StatefulWidget allows the UI to change
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String displayText = "Hello Flutter!"; // initial text
+
+  void toggleText() {
+    setState(() {
+      if (displayText == "Hello Flutter!") {
+        displayText = "You pressed the button";
+      } else {
+        displayText = "hello world";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    fullName("sai", "charan");
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Interactive App')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(displayText, style: TextStyle(fontSize: 24)),
+              SizedBox(height: 20), // adds space between text and button
+              ElevatedButton(onPressed: toggleText, child: Text("Press Me")),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
